706b3583abedc9f0f9f44b94e6397727f6100961
JP Author <91775790+pogprog@users.noreply.github.com>
2024-06-04 14:21:08 +0800

    ghfgg

--- app/src/main/java/com/example/collaboratech_hackathon/JobAcceptTask.java
+++ app/src/main/java/com/example/collaboratech_hackathon/JobAcceptTask.java
@@ -2,7 +2,13 @@ package com.example.collaboratech_hackathon;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.widget.TextView;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +23,17 @@ public class JobAcceptTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_job_accept_task);
+
+        ImageButton btn = (ImageButton)findViewById(R.id.imageButton);
+        //Intent bkbutton = new Intent();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(JobAcceptTask.this, homefrag.class));
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/collaboratech_hackathon/ReleaseMoneyAct.java
+++ app/src/main/java/com/example/collaboratech_hackathon/ReleaseMoneyAct.java
@@ -1,6 +1,10 @@
 package com.example.collaboratech_hackathon;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +21,18 @@ public class ReleaseMoneyAct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_release_money);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageButton button = findViewById(R.id.imageButton2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReleaseMoneyAct.this, Manage.class);
+                startActivity(intent);
+            }
         });
+
+
 
     }
 
